3b3986ef0835c40cb6f1e6d29a19a1ccda58fe3f
Badr bellaj <bellaj.badr@gmail.com>
2021-08-16 22:02:20 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,14 @@
 #docker network disconnect -f fabric_test logspout
 docker kill $(docker ps -q) # added for deploying cc
 source ./configureHyperledger.sh
-echo "===============================up ======================="
+echo "=============================== Stopping previous instances ======================="
 ./network.sh down
+echo "===============================Create channel with CA and CouchDb ======================="
+
 ./network.sh up createChannel -ca -s couchdb
-echo "=====================Deploying CC======================="
-echo "#"
-echo "# Starting CC deployment"
-echo "========================================================"
-./network.sh deployCC -ccn cpuTokenChainC3 -ccp ../../tangerMed/cpuChainCode -ccv 1 -ccl javascript 
+#echo "=====================Deploying CC======================="
+#echo "#"
+#echo "# Starting CC deployment"
+#echo "========================================================"
+#./network.sh deployCC -ccn cpuTokenChainC3 -ccp ../../tangerMed/cpuChainCode -ccv 1 -ccl javascript 
 
