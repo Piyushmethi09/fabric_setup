b4f75e09d6e8a1441f26934a8eaa8d0814dfb089
bellaj <bellaj.badr@gmail.com>
2021-08-24 11:43:52 +0100

    up config files

--- configureHyperledger.sh
+++ configureHyperledger.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export FABRIC_CFG_PATH=${PWD/..}/config/
+export FABRIC_CFG_PATH=${PWD}/../config/
 export CORE_PEER_TLS_ENABLED=true
 export CORE_PEER_LOCALMSPID="Org1MSP"
 export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #docker network disconnect -f fabric_test logspout
-docker kill $(docker ps -q) # added for deploying cc
+#docker kill $(docker ps -q) # added for deploying cc
+echo "===============================setting UP fabric variables and using peer 0 ============="
 source ./configureHyperledger.sh
-echo "=============================== Stopping previous instances ======================="
+echo "=============================== Stopping previous instances ============================="
 ./network.sh down
 echo "===============================Create channel with CA and CouchDb ======================="
-
 ./network.sh up createChannel -ca -s couchdb
 #echo "=====================Deploying CC======================="
 #echo "#"
