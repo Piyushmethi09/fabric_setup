0976942097668eda8474843a517ca3c65235bd10
bellaj <bellaj.badr@gmail.com>
2021-08-24 11:56:47 +0100

    up script

--- start.sh
+++ start.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-#docker network disconnect -f fabric_test logspout
-#docker kill $(docker ps -q) # added for deploying cc
-echo "===============================setting UP fabric variables and using peer 0 ============="
+
+echo "===============================setting UP fabric variables and using peer 0 ========================"
+## setting up env variables to use peer0 would enable us to execut commandes directly from shell to interact with the network
 source ./configureHyperledger.sh
-echo $FABRIC_CFG_PATH
-echo "=============================== Stopping previous instances ============================="
+echo "Fabric binaries path is set to $FABRIC_CFG_PATH"
+echo "=============================== Stopping previous instances ========================================"
 ./network.sh down
-echo "===============================Create default channel with CAs and CouchDb==============="
+echo "===============================Create default channel(mychannel) with CAs and CouchDb==============="
 ./network.sh up createChannel -ca -s couchdb
-#echo "=====================Deploying CC======================="
-#echo "#"
-#echo "# Starting CC deployment"
-#echo "========================================================"
-#./network.sh deployCC -ccn cpuTokenChainC3 -ccp ../../tangerMed/chaincode -ccv 1 -ccl javascript 
+#echo "===============================Deploying CC========================================================"
+#./network.sh deployCC -ccn chaincodename -ccp ../../chaincode/Path -ccv 1 -ccl javascript 
 
